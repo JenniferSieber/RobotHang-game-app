cca96f0101e277759f1c8f8eeb0f01d3375c7fe7
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2023-10-13 17:29:27 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,7 @@ import { faTwitter, faInstagram, faLinkedin, faGithub } from '@fortawesome/free-
 function Footer() {
   return (
     <footer className="footer">
-      <section className="footer-container">   
-        
+      <section className="footer-container">     
         <div>
           <a href="#" className="brand-sm"> 
             <FontAwesomeIcon icon={faRobot} />
@@ -15,24 +14,26 @@ function Footer() {
         </div>            
   
         <div className="social-icons-container">
-          <a href="https://twitter.com/JenWebDev">
+          <a href="https://twitter.com/JenWebDev" target="_blank">
             <FontAwesomeIcon icon={faTwitter} className="social-icons" />
           </a>
-          <a href="https://www.instagram.com/jennifer_webdev/">
+
+          <a href="https://www.instagram.com/jennifer_webdev/" target="_blank">
             <FontAwesomeIcon icon={faInstagram} className="social-icons" />
           </a>
-          <a href="https://www.linkedin.com/in/jennifer-sieber-74a54726b/">
+
+          <a href="https://www.linkedin.com/in/jennifer-sieber-74a54726b/" target="_blank">
             <FontAwesomeIcon icon={faLinkedin} className="social-icons" />
           </a>
-          <a href="https://www.linkedin.com/in/jennifer-sieber-74a54726b/">
+
+          <a href="https:/https://github.com/JenniferSieber/" target="_blank">
             <FontAwesomeIcon icon={faGithub} className="social-icons" />
           </a>
         </div>
-        
+
         <div className="dev">
           <a href="https://https://github.com/JenniferSieber" className="dev-link" target="_blank">created by: Jennifer Sieber &copy;2023</a>
         </div> 
-        
       </section>
     </footer>
   )
